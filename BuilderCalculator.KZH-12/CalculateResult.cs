e813c47e9f5bc076dd1bf2ffff238c8c8ebd76f0
Eridani 137 <tsuuzetsu@gmail.com>
2025-06-06 01:57:40 +0300

    ✨ feat(BuilderCalculator.KZH-12): add new project with calculator functionality and initial implementation files

---
+++ BuilderCalculator.KZH-12/CalculateResult.cs
@@ -0,0 +1,16 @@
+﻿using Calculators.Shared.Abstractions;
+
+namespace BuilderCalculator.KZH_12
+{
+    public class CalculateResult : BaseCalculateResult
+    {
+        public CalculateResult(BaseBuilderCalculator calculator) : base(calculator)
+        {
+        }
+
+        public override void PrintSummary()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BuilderCalculator.KZH-12/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using Calculators.Shared.Abstractions;
+
+namespace BuilderCalculator.KZH_12
+{
+    public class Calculator : BaseBuilderCalculator
+    {
+        private CalculateResult CalculateResult { get; set; }
+
+        public Calculator()
+        {
+            CalculateResult = new CalculateResult(this);
+        }
+        
+        
+        
+        public override BaseCalculateResult Calculate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
